707dc154146d6af5724ebdd3dc26751a29836cf1
JillesMe <info@jilles.me>
2014-06-11 16:15:20 +0200

    4 spaces -> 2 spaces

--- adblockcheck.js
+++ adblockcheck.js
@@ -1,36 +1,36 @@
 /*! adblockdetect.js v1.0.0 | (c) 2014 @jilles | https://github.com/jillesme/AdBlockCheck */
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(factory);
-    } else if (typeof exports === 'object') {
-        module.exports = factory;
-    } else {
-        root.adblock = factory();
-    }
+  if (typeof define === 'function' && define.amd) {
+    define(factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory;
+  } else {
+    root.adblock = factory();
+  }
 })(this, function () {
 
-    'use strict';
+  'use strict';
 
-    var adblock = {};
+  var adblock = {};
 
-    var createAd = function () {
-        var ad = document.createElement('div');
+  var createAd = function () {
+    var ad = document.createElement('div');
 
-        ad.id = 'adcontent';
-        ad.style.display = 'none';
+    ad.id = 'adcontent';
+    ad.style.display = 'none';
 
-        document.body.appendChild(ad);
+    document.body.appendChild(ad);
 
-    };
+  };
 
-    adblock.check = function (callback, delay) {
-        createAd();
+  adblock.check = function (callback, delay) {
+    createAd();
 
-        setTimeout(function () {
-            callback(!!document.getElementById('adcontent'));
-        }, delay || 1000);
+    setTimeout(function () {
+      callback(!!document.getElementById('adcontent'));
+    }, delay || 1000);
 
-    };
+  };
 
-    return adblock;
+  return adblock;
 });
